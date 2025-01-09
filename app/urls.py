c776ff06d600f436804335bf1f7928e39329542c
Teguh Samudra <teguhsam9@gmail.com>
2025-01-09 13:42:27 -0800

    cleaning up our article URLs

--- app/urls.py
+++ app/urls.py
@@ -8,11 +8,7 @@ from app.views import (
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="home"),
-    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
-    path(
-        "articles/<int:pk>/update", ArticleUpdateView.as_view(), name="update_article"
-    ),
-    path(
-        "articles/<int:pk>/delete", ArticleDeleteView.as_view(), name="delete_article"
-    ),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("<int:pk>/update", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete", ArticleDeleteView.as_view(), name="delete_article"),
 ]
