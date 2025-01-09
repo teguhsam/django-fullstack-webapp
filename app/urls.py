31f15ceef739c8e889a3b799f5edd81ec186114a
Teguh Samudra <teguhsam9@gmail.com>
2025-01-09 13:39:11 -0800

    adding the other app endpoints with CBV

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from app.views import home, ArticleCreateView
+from app.views import (
+    ArticleCreateView,
+    ArticleUpdateView,
+    ArticleDeleteView,
+    ArticleListView,
+)
 
 urlpatterns = [
-    path("", home, name="home"),
+    path("", ArticleListView.as_view(), name="home"),
     path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
+    path(
+        "articles/<int:pk>/update", ArticleUpdateView.as_view(), name="update_article"
+    ),
+    path(
+        "articles/<int:pk>/delete", ArticleDeleteView.as_view(), name="delete_article"
+    ),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app.models import Article
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -9,8 +9,29 @@ def home(request):
     return render(request, "app/home.html", {"articles": articles})
 
 
+class ArticleListView(ListView):
+    template_name = "app/home.html"
+    model = Article
+    context_object_name = "articles"
+
+
 class ArticleCreateView(CreateView):
     template_name = "app/article_create.html"
     model = Article
     fields = ["title", "status", "content", "word_count", "twitter_post"]
     success_url = reverse_lazy("home")
+
+
+class ArticleUpdateView(UpdateView):
+    template_name = "app/article_update.html"
+    model = Article
+    fields = ["title", "status", "content", "word_count", "twitter_post"]
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
+
+
+class ArticleDeleteView(DeleteView):
+    template_name = "app/article_delete.html"
+    model = Article
+    success_url = reverse_lazy("home")
+    context_object_name = "article"
