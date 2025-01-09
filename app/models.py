309ea8da333e2e9bb5a6db125a748e6ee61eeea5
Teguh Samudra <teguhsam9@gmail.com>
2025-01-09 14:25:43 -0800

    calculating the word count dynamically in articles

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import re
 
 ARTICLE_STATUS = (
     ("draft", "draft"),
@@ -24,3 +25,8 @@ class Article(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        text = re.sub(r"<[^>]*", "", self.content).replace("&nbsp;", " ")
+        self.word_count = len(re.findall(r"\b\w+\b"), text)
+        super().save(*args, **kwargs)
