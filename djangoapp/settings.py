fb00b050aa60d5da4b2c8cedd4cffa7d183da5e5
Teguh Samudra <teguhsam9@gmail.com>
2025-01-12 11:08:41 -0800

    add login and logout pages

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -116,6 +116,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("articles/", include("app.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
