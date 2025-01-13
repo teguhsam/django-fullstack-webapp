df27fb75611a8e7f3d8cc2934562e4621e76c9e9
Teguh Samudra <teguhsam9@gmail.com>
2025-01-13 10:40:35 -0800

    customize the UserProfile admin page

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from app.models import Article, UserProfile
 
 
@@ -11,5 +12,38 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ("word_count", "created_at", "updated_at")
 
 
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "fields": ("email", "password1", "password2"),
+                "classes": ("wide",),
+            },
+        ),
+    )
+    list_display = ("email", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active")
+    search_field = ("email",)
+    ordering = ("email",)
+
+
 admin.site.register(Article, ArticleAdmin)
-admin.site.register(UserProfile)
+admin.site.register(UserProfile, CustomUserAdmin)
