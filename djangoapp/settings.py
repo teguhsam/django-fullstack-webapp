5cdcc1189ce7761a664da9e959ab90b5e263c1b4
Teguh Samudra <teguhsam9@gmail.com>
2025-01-14 10:48:00 -0800

    add dependencies and switch to postgres

--- djangoapp/settings.py
+++ djangoapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -103,12 +104,7 @@ WSGI_APPLICATION = "djangoapp.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": dj_database_url.config(conn_max_age=600)}
 
 # Customize user model
 AUTH_USER_MODEL = "app.UserProfile"
