51bed1711fc668e1f1b4f70a0f2473a70c0b3c94
Teguh Samudra <teguhsam9@gmail.com>
2025-02-17 09:58:14 -0800

    first automated browser test with Playwright

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 
 
-def test_can_load_homepage(test_server: Page):
-    test_server.goto("/")
-    assert "Write and collaborate" in test_server.content()
+def test_can_load_homepage(page: Page):
+    page.goto("/")
+    assert "Write and collaborate" in page.content()
